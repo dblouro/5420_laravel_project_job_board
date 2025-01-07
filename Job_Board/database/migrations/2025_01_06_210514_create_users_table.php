6fc6f438221032068faee2a72f45fc8a4a1cca3d
Ricardo Conceição <75455972+runniOp@users.noreply.github.com>
2025-01-07 19:23:28 +0000

    Fix dos erros nas migre

--- Job_Board/database/migrations/2025_01_06_210514_create_users_table.php
+++ Job_Board/database/migrations/2025_01_06_210514_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password'); // Adicione esta linha
+            $table->rememberToken();   // Adicione esta linha para suporte ao token de "lembrar-me"
             $table->timestamps();
         });
     }
--- Job_Board/database/migrations/2025_01_06_210840_create_companies_table.php
+++ Job_Board/database/migrations/2025_01_06_210840_create_companies_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('address'); // Adicione esta linha
+            $table->string('website')->nullable();
             $table->timestamps();
         });
     }
