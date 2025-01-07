744d9d10688febd73d0d2e4410e4fb5e84b40ed0
Ricardo Conceição <75455972+runniOp@users.noreply.github.com>
2025-01-07 03:37:24 +0000

    Template feito

---
+++ Job_Board/resources/views/partials/footer.blade.php
@@ -0,0 +1,12 @@
+<div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5">
+    <div class="container py-5">
+        <div class="row g-5">
+            <div class="col-lg-3 col-md-6">
+                <h5 class="text-white mb-4">Company</h5>
+                <a class="btn btn-link text-white-50" href="#">About Us</a>
+                <a class="btn btn-link text-white-50" href="#">Contact Us</a>
+            </div>
+            <!-- Adicione mais conteúdo conforme necessário -->
+        </div>
+    </div>
+</div>
--- Job_Board/routes/web.php
+++ Job_Board/routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/job-list', function () {
+    return view('job-list');
+});
+
+Route::get('/job-detail', function () {
+    return view('job-detail');
 });
