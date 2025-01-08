6ef38c8d0b7b17cf045b9946b80a3d5d4c2ae4a5
Diogo Louro <Diogo.Louro.T0127776@edu.atec.pt>
2025-01-08 23:16:25 +0000

    fix_searchbar_home

--- Job_Board/resources/views/home.blade.php
+++ Job_Board/resources/views/home.blade.php
@@ -43,7 +43,7 @@
         <!-- Search Start -->
         <div class="container-fluid bg-primary mb-5 wow fadeIn" data-wow-delay="0.1s" style="padding: 35px;">
             <div class="container">
-                <form action="{{ route('jobs.search') }}" method="GET">
+                <form action="{{ route('jobs.index') }}" method="GET">
                     <div class="row g-2">
                         <div class="col-md-10">
                             <div class="row g-2">
